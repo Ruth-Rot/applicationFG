89131e16fbc6513af043ae0f2fe40d112bb1464a
Noamls123 <73317511+Noamls123@users.noreply.github.com>
2021-04-07 10:37:17 +0300

    Add files via upload

---
+++ controls/Panel.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace app0.controls
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class Panel : UserControl
+    {
+
+        public Panel()
+        {
+            InitializeComponent();
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+
+        private void Slider2_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
+        }
+       
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            //VM_Stop = false;
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            //model.Stop();
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlaySpeed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Speed_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        
+    }
+}

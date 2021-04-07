09ded3c90a6fb684740247aa787e1d688eaa3ddc
shaked-arel <72872740+shaked-arel@users.noreply.github.com>
2021-04-07 10:32:37 +0300

    Add files via upload

---
+++ Joystick.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ass1
+{
+    /// <summary>
+    /// Interaction logic for Joystick.xaml
+    /// </summary>
+    public partial class Joystick : UserControl
+    {
+        public Joystick()
+        {
+            InitializeComponent();
+        }
+
+        void Move(Double x,Double y)
+        {
+
+        }
+
+        void up()
+        {
+            knobPosition.Y += 20;
+        }
+
+        void Down()
+        {
+            knobPosition.Y += 20;
+        }
+
+        void Left()
+        {
+            knobPosition.X += 20;
+        }
+
+        void Right()
+        {
+            knobPosition.X += 20;
+        }
+
+    }
+}

b4264205d5bed53a96716800826a49498fec1714
Noamls123 <73317511+Noamls123@users.noreply.github.com>
2021-04-08 10:27:42 +0300

    Add files via upload

--- controls/Panel.xaml.cs
+++ controls/Panel.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,10 +21,16 @@ namespace app0.controls
     /// </summary>
     public partial class Panel : UserControl
     {
+        Model model;
+        ViewModel vm;
+        View view;
 
         public Panel()
         {
             InitializeComponent();
+            model = new Model();
+            vm = new ViewModel(model);
+            this.DataContext = vm;
         }
 
         private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -38,17 +45,18 @@ namespace app0.controls
        
         private void Play_Click(object sender, RoutedEventArgs e)
         {
-            //VM_Stop = false;
+            vm.VM_Sleep = 100;
+
         }
 
         private void Stop_Click(object sender, RoutedEventArgs e)
         {
-            //model.Stop();
+            vm.VM_Sleep = -1;
         }
 
         private void Go_Click(object sender, RoutedEventArgs e)
         {
-
+            
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
@@ -65,6 +73,20 @@ namespace app0.controls
         {
 
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
         
+
+        private void Clock_changed(object sender, TextChangedEventArgs e)
+        {
+            double time = 0.1 * vm.VM_Line_num;
+            string strTime = time.ToString();
+            DateTime dt = DateTime.ParseExact(strTime, "HHmm", CultureInfo.InvariantCulture);
+            string timestring = dt.ToString("H:mm:ss");
+        }
     }
 }

c98796dc73e4d16cc3f3d4dbde3f8bed7fe285a7
shaked-arel <72872740+shaked-arel@users.noreply.github.com>
2021-04-11 10:46:43 +0300

    Add files via upload

---
+++ Model.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using System.Xml.Resolvers;
+
+
+namespace app0
+{
+    /// <summary>
+    /// Interaction logic for ConnectServer.xaml
+    /// </summary>
+    public class Model : IModel
+    {
+        //private LoadPath load;
+
+        // flight controls
+        private string file_path;
+        private string xml_path;
+        private string time_passed;
+        private double time_in_num;
+
+        private List<String> xmlNames;
+
+        private float aileron;
+        private float elevator;
+        private float rudder;
+        private float flaps;
+        private float slats;
+        private float speed_brake;
+        private float heading;
+        private float pitch;
+        private float yaw;
+        private float roll;
+        private float airSpeed;
+        private float altimeter;
+
+        //engines
+        private float throttle;
+
+        //gear hydraulics
+        private float engine_pump;
+        private float electric_pump;
+
+        //electric
+        private float external_power;
+        private float apu_generator;
+
+        //autoflight position
+        private float latitude_deg;
+        private float longitude_deg;
+        private float altitude_ft;
+
+        //orientation
+        /*private float roll_deg;
+        private float pitch_deg;
+        private float heading_deg;
+        private float side_slip_deg;
+        //velocities
+        private float airspeed_kt;
+        private float glideslope;
+        private float vertical_speed_fps;
+        //accelerations, surface positions, instruments
+        private float airspeed_indicator_indicated_speed_kt;
+        private float altimeter_indicated_altitude_ft;
+        private float altimeter_pressure_alt_ft;
+        private float attitude_indicator_indicated_pitch_deg;
+        private float attitude_indicator_indicated_roll_deg;
+        private float attitude_indicator_internal_pitch_deg;
+        private float attitude_indicator_internal_roll_deg;
+        private float encoder_indicated_altitude_ft;
+        private float encoder_pressure_alt_ft;
+        private float gps_indicated_altitude_ft;
+        private float gps_indicated_ground_speed_kt;
+        private float gps_indicated_vertical_speed;
+        private float indicated_heading_deg;
+        private float magnetic_compass_indicated_heading_deg;
+        private float slip_skid_ball_indicated_slip_skid;*/
+
+        //volatile Boolean stop;
+
+        int timeWait;
+        private List<string> file;
+        int current_line;
+        int num_of_lines;
+        Boolean stop;
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Model(String csv, String xml)
+        {
+            file_path = csv;
+            xml_path = xml;
+            stop = false;
+            current_line = 0;
+            timeWait = 100;
+            time_in_num = 0;
+            file = new List<string>(42);
+            SaveFile();
+            SaveXml();
+            ConnectFg();
+        }
+
+        public void ConnectFg()
+        {
+            var client = new TcpClient("localhost", 5400);
+            NetworkStream ns = client.GetStream();
+            string line;
+
+            new Thread(delegate ()
+            {
+                line = file[current_line] + "\r\n";
+                while (current_line != num_of_lines+1)
+                {
+                    if (current_line != num_of_lines + 1)
+                    {
+                        line = file[current_line] + "\r\n";
+                    }
+                    if (!stop&&(current_line!=num_of_lines))
+                    {
+                        current_line++;
+                        InitialProperties();
+                        NotifyPropertyChanged("Current_line");
+                        time_in_num = (current_line / 10);
+                        time_passed = TimeSpan.FromSeconds(time_in_num).ToString();
+                        NotifyPropertyChanged("TimePassed");
+                    }
+                    //add line to list
+                    // change linenum
+                    Console.WriteLine(line);
+                    //InitialProperties();
+                    ns.Write(System.Text.Encoding.ASCII.GetBytes(line), 0, System.Text.Encoding.ASCII.GetBytes(line).Length);
+                    ns.Flush();
+                    Thread.Sleep(timeWait);
+                }
+            }).Start();
+        }
+
+
+        public void SaveFile()
+        {
+            var bufferr = new System.IO.StreamReader(file_path);
+            string line;
+            while ((line = bufferr.ReadLine()) != null)
+            {
+                file.Add(line);
+            }
+            var lineCount = File.ReadLines(file_path).Count();
+            num_of_lines = lineCount;
+        }
+
+        public void SaveXml()
+        {
+            XDocument xml = XDocument.Load(xml_path);
+            IEnumerable<string> temp = xml.Descendants("output").Descendants("name").Select(name => (string)name);
+            xmlNames = temp.ToList();
+        }
+
+        private void InitialProperties()
+        {
+            String[] arrProperties = file[current_line].Split(',');
+            Aileron = float.Parse(arrProperties[xmlNames.IndexOf("aileron")]);
+            Elevator = float.Parse(arrProperties[xmlNames.IndexOf("elevator")]);
+            Rudder = float.Parse(arrProperties[xmlNames.IndexOf("rudder")]);
+            Throttle = float.Parse(arrProperties[xmlNames.IndexOf("throttle")]);
+            Altimeter = float.Parse(arrProperties[xmlNames.IndexOf("altitude-ft")]);
+            AirSpeed = float.Parse(arrProperties[xmlNames.IndexOf("airspeed-kt")]);
+            Heading = float.Parse(arrProperties[xmlNames.IndexOf("heading-deg")]);
+            Pitch = float.Parse(arrProperties[xmlNames.IndexOf("pitch-deg")]);
+            Yaw = float.Parse(arrProperties[xmlNames.IndexOf("side-slip-deg")]);
+            Roll = float.Parse(arrProperties[xmlNames.IndexOf("roll-deg")]);
+        }
+
+        public void NotifyPropertyChanged(String propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        public float Aileron
+        {
+            get
+            {
+                return aileron * 60 +50;
+            }
+            set
+            {
+                aileron = value;
+                Console.WriteLine(aileron+"\n");
+                NotifyPropertyChanged("Aileron");
+            }
+        }
+
+        public float Elevator
+        {
+            set
+            {
+                elevator = value;
+                NotifyPropertyChanged("Elevator");
+            }
+            get
+            {
+                return elevator * 60+50 ;
+            }
+        }
+        public float Rudder
+        {
+            set
+            {
+                rudder = value;
+                NotifyPropertyChanged("Rudder");
+            }
+            get
+            {
+                return rudder * 200+20;
+            }
+        }
+        public float Throttle
+        {
+            set
+            {
+                throttle = value;
+                NotifyPropertyChanged("Throttle");
+            }
+            get
+            {
+                return throttle*200-20 ;
+            }
+        }
+
+        public float Altimeter
+        {
+            set
+            {
+                altimeter = value;
+                NotifyPropertyChanged("Altimeter");
+            }
+            get
+            {
+                return altimeter;
+            }
+        }
+        public float AirSpeed
+        {
+            set
+            {
+                airSpeed = value;
+                NotifyPropertyChanged("AirSpeed");
+            }
+            get
+            {
+                return airSpeed;
+            }
+        }
+        public float Pitch
+        {
+            set
+            {
+                pitch = value;
+                NotifyPropertyChanged("Pitch");
+            }
+            get
+            {
+                return pitch;
+            }
+        }
+        public float Roll
+        {
+            set
+            {
+                roll = value;
+                NotifyPropertyChanged("Roll");
+            }
+            get
+            {
+                return roll;
+            }
+        }
+        public float Yaw
+        {
+            set
+            {
+                yaw = value- 0.5;
+                NotifyPropertyChanged("Yaw");
+            }
+            get
+            {
+                return yaw;
+            }
+        }
+        public float Heading
+        {
+            set
+            {
+                heading = value;
+                NotifyPropertyChanged("Heading");
+            }
+            get
+            {
+                return heading;
+            }
+        }
+
+        public int Current_line
+        {
+            get
+            {
+                return current_line;
+            }
+            set
+            {
+                current_line = value;
+                NotifyPropertyChanged("Current_line");
+            }
+        }
+        public int Num_of_lines
+        {
+            get
+            {
+                return num_of_lines;
+            }
+            set
+            {
+                num_of_lines = value;
+                NotifyPropertyChanged("Num_of_lines");
+            }
+        }
+        public Boolean Stop
+        {
+            get
+            {
+                return stop;
+            }
+            set
+            {
+                stop = value;
+                NotifyPropertyChanged("Stop");
+            }
+        }
+        public int Sleep
+        {
+            get
+            {
+                return timeWait;
+            }
+            set
+            {
+                timeWait = value;
+                NotifyPropertyChanged("Sleep");
+            }
+        }
+
+        public string File_path { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string TimePassed
+        {
+            get
+            {
+                return time_passed;
+            }
+            set
+            {
+                time_passed = value;
+                NotifyPropertyChanged("TimePassed");
+            }
+        }
+
+        //public string File_path { set; get; }
+    }
+
+}
---
+++ View.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+
+namespace app0
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class View : Window
+    {
+        private ViewModel vm;
+        private String filePath;
+        private String xmlFile;
+
+
+        public View(String csv, String xml)
+        {
+            InitializeComponent();
+            filePath = csv;
+            xmlFile = xml;
+            ViewModel vm = new ViewModel(new Model(csv, xml));
+            MediaBar media = new MediaBar(vm);
+            Joystick joy = new Joystick(vm);
+            measurments meas = new measurments(vm);
+            grd_panel.Children.Add(media);
+            joystick_panel.Children.Add(joy);
+            measurments_panel.Children.Add(meas);
+            DataContext = vm;
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //NavigationService.Navigate(home);
+                this.filePath = System.IO.Path.GetFullPath(openFileDialog.FileName);
+
+                vm = new ViewModel(new Model(filePath,xmlFile));
+                vm.VM_File_path = filePath;
+                DataContext = vm;
+                
+
+                //vm = new ViewModel(new Model(filePath));
+                //DataContext = vm;
+            }
+        }
+
+     
+        public string Filepath
+        {
+            get { return filePath; }
+        }
+    }
+}
